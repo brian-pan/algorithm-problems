5359aa373229c6b9ddbb7104f83ebbc3489baa81
Brian Pan <brianpanzifan@gmail.com>
2022-06-01 15:49:44 -0400

    more solutions

--- detectPangram.js
+++ detectPangram.js
@@ -17,4 +17,10 @@ function isPangram(string) {
   return bool;
 }
 
-//
+// Other solution:
+function isPangram(string) {
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
+}
