42112fcd0e2a78c8395898f7f971dd7a062b2224
Brian Pan <brianpanzifan@gmail.com>
2022-06-07 12:52:48 -0400

    updated question

--- findParityOutlier.js
+++ findParityOutlier.js
@@ -21,3 +21,23 @@ function findOutlier(integers) {
   });
   return odds.length === 1 ? odds[0] : evens[0];
 }
+
+//other solutions:
+function findOutlier(int) {
+  var even = int.filter((a) => a % 2 == 0);
+  var odd = int.filter((a) => a % 2 !== 0);
+  return even.length == 1 ? even[0] : odd[0];
+}
+
+//other solutions:
+function findOutlier(integers) {
+  return integers.slice(0, 3).filter(even).length >= 2
+    ? integers.find(odd)
+    : integers.find(even);
+}
+function even(num) {
+  return num % 2 == 0;
+}
+function odd(num) {
+  return !even(num);
+}
